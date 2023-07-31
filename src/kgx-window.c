6c55fddd8782ed255b1031de688551786176761e
Zander Brown <zbrown@gnome.org>
2023-07-31 01:50:21 +0100

    window: take full advantage of gtk4
     - where possible use GtkExpression for bindings
     - use widget actions instead of action map

--- src/kgx-window.c
+++ src/kgx-window.c
@@ -36,7 +36,6 @@
 /**
  * KgxWindow:
  * @close_anyway: ignore running children and close without prompt
- * @zoom_level: the #GtkLabel in the #GtkPopover showing the current zoom level
  * @pages: the #KgxPages of #KgxPage current in the window
  *
  * The main #AdwApplicationWindow that acts as the terminal
@@ -51,14 +50,10 @@ struct _KgxWindowPrivate {
   gboolean              close_anyway;
 
   /* Template widgets */
-  GtkWidget            *window_title;
   GtkWidget            *theme_switcher;
-  GtkWidget            *zoom_level;
   GtkWidget            *tab_bar;
   GtkWidget            *tab_overview;
   GtkWidget            *pages;
-
-  GActionMap           *tab_actions;
 };
 
 
@@ -81,7 +76,6 @@ kgx_window_dispose (GObject *object)
   KgxWindowPrivate *priv = kgx_window_get_instance_private (self);
 
   g_clear_object (&priv->settings);
-  g_clear_object (&priv->tab_actions);
 
   G_OBJECT_CLASS (kgx_window_parent_class)->dispose (object);
 }
@@ -297,17 +291,12 @@ extra_drag_drop (AdwTabBar        *bar,
 }
 
 
-static void new_tab_activated (GSimpleAction *action,
-                               GVariant      *parameter,
-                               gpointer       data);
-
-
 static AdwTabPage *
 create_tab_cb (KgxWindow *self)
 {
   KgxWindowPrivate *priv = kgx_window_get_instance_private (self);
 
-  new_tab_activated (NULL, NULL, self);
+  gtk_widget_activate_action (GTK_WIDGET (self), "win.new-tab", NULL);
 
   return kgx_pages_get_selected_page (KGX_PAGES (priv->pages));
 }
@@ -316,9 +305,9 @@ create_tab_cb (KgxWindow *self)
 static void
 breakpoint_applied (KgxWindow *self)
 {
-  GAction *action = g_action_map_lookup_action (G_ACTION_MAP (self), "show-tabs-desktop");
-
-  g_simple_action_set_enabled (G_SIMPLE_ACTION (action), FALSE);
+  gtk_widget_action_set_enabled (GTK_WIDGET (self),
+                                 "win.show-tabs-desktop",
+                                 FALSE);
 }
 
 
@@ -326,13 +315,185 @@ static void
 breakpoint_unapplied (KgxWindow *self)
 {
   KgxWindowPrivate *priv = kgx_window_get_instance_private (self);
-  GAction *action = g_action_map_lookup_action (G_ACTION_MAP (self), "show-tabs-desktop");
 
-  g_simple_action_set_enabled (G_SIMPLE_ACTION (action), TRUE);
+  gtk_widget_action_set_enabled (GTK_WIDGET (self),
+                                 "win.show-tabs-desktop",
+                                 TRUE);
 
   if (kgx_pages_count (KGX_PAGES (priv->pages)) > 0) {
     adw_tab_overview_set_open (ADW_TAB_OVERVIEW (priv->tab_overview), FALSE);
   }
+}
+
+
+static char *
+title_or_fallback (GObject *object, const char *title)
+{
+  if (G_UNLIKELY (!title || !title[0])) {
+    return g_strdup (KGX_DISPLAY_NAME);
+  }
+
+  return g_strdup (title);
+}
+
+
+static char *
+path_as_subtitle (GObject *object, GFile *file)
+{
+  g_autofree char *path = NULL;
+  const char *home = NULL;
+
+  if (!file) {
+    return NULL;
+  }
+
+  path = g_file_get_path (file);
+  if (G_UNLIKELY (!path)) {
+    return NULL;
+  }
+
+  home = g_get_home_dir ();
+  if (G_LIKELY (g_str_has_prefix (path, home))) {
+    g_autofree char *short_home = g_strdup_printf ("~%s",
+                                                   path + strlen (home));
+
+    return g_steal_pointer (&short_home);
+  }
+
+  return g_steal_pointer (&path);
+}
+
+
+static char *
+scale_as_label (GObject *object, double scale)
+{
+  return g_strdup_printf ("%i%%", (int) round (scale * 100));
+}
+
+
+static void
+close_tab_activated (GtkWidget  *widget,
+                     const char *action_name,
+                     GVariant   *parameter)
+{
+  KgxWindow *self = KGX_WINDOW (widget);
+  KgxWindowPrivate *priv = kgx_window_get_instance_private (self);
+
+  kgx_pages_close_page (KGX_PAGES (priv->pages));
+}
+
+
+static void
+detach_tab_activated (GtkWidget  *widget,
+                      const char *action_name,
+                      GVariant   *parameter)
+{
+  KgxWindow *self = KGX_WINDOW (widget);
+  KgxWindowPrivate *priv = kgx_window_get_instance_private (self);
+
+  kgx_pages_detach_page (KGX_PAGES (priv->pages));
+}
+
+
+static void
+new_activated (GtkWidget  *widget,
+               const char *action_name,
+               GVariant   *parameter)
+{
+  KgxWindow *self = KGX_WINDOW (widget);
+  guint32 timestamp = GDK_CURRENT_TIME;
+  GtkApplication *application = NULL;
+  g_autoptr (GFile) dir = NULL;
+
+  application = gtk_window_get_application (GTK_WINDOW (self));
+  dir = kgx_window_get_working_dir (self);
+
+  kgx_application_add_terminal (KGX_APPLICATION (application),
+                                NULL,
+                                timestamp,
+                                dir,
+                                NULL,
+                                NULL);
+}
+
+
+static void
+new_tab_activated (GtkWidget  *widget,
+                   const char *action_name,
+                   GVariant   *parameter)
+{
+  KgxWindow *self = KGX_WINDOW (widget);
+  guint32 timestamp = GDK_CURRENT_TIME;
+  GtkApplication *application = NULL;
+  g_autoptr (GFile) dir = NULL;
+
+  application = gtk_window_get_application (GTK_WINDOW (self));
+  dir = kgx_window_get_working_dir (self);
+
+  kgx_application_add_terminal (KGX_APPLICATION (application),
+                                self,
+                                timestamp,
+                                dir,
+                                NULL,
+                                NULL);
+}
+
+
+static void
+about_activated  (GtkWidget  *widget,
+                  const char *action_name,
+                  GVariant   *parameter)
+{
+  const char *developers[] = { "Zander Brown <zbrown@gnome.org>", NULL };
+  const char *designers[] = { "Tobias Bernard", NULL };
+  g_autofree char *copyright = NULL;
+
+  /* Translators: %s is the year range */
+  copyright = g_strdup_printf (_("© %s Zander Brown"), "2019-2023");
+
+  adw_show_about_window (GTK_WINDOW (widget),
+                         "application-name", KGX_DISPLAY_NAME,
+                         "application-icon", KGX_APPLICATION_ID,
+                         "developer-name", _("The GNOME Project"),
+                         "issue-url", "https://gitlab.gnome.org/GNOME/console/-/issues/new",
+                         "website", "https://apps.gnome.org/en-GB/app/org.gnome.Console/",
+                         "version", PACKAGE_VERSION,
+                         "developers", developers,
+                         "designers", designers,
+                         /* Translators: Credit yourself here */
+                         "translator-credits", _("translator-credits"),
+                         "copyright", copyright,
+                         "license-type", GTK_LICENSE_GPL_3_0,
+                         NULL);
+}
+
+
+static void
+tab_switcher_activated (GtkWidget  *widget,
+                                   const char *action_name,
+                                   GVariant   *parameter)
+{
+  KgxWindow *self = KGX_WINDOW (widget);
+  KgxWindowPrivate *priv = kgx_window_get_instance_private (self);
+
+  adw_tab_overview_set_open (ADW_TAB_OVERVIEW (priv->tab_overview), TRUE);
+}
+
+
+static void
+show_preferences_window_activated (GtkWidget  *widget,
+                                   const char *action_name,
+                                   GVariant   *parameter)
+{
+  KgxWindow *self = KGX_WINDOW (widget);
+  KgxWindowPrivate *priv = kgx_window_get_instance_private (self);
+  GtkWindow *preferences;
+
+  preferences = g_object_new (KGX_TYPE_PREFERENCES_WINDOW,
+                              "transient-for", self,
+                              "settings", priv->settings,
+                              NULL);
+  gtk_window_present (preferences);
 }
 
 
@@ -368,9 +529,7 @@ kgx_window_class_init (KgxWindowClass *klass)
   gtk_widget_class_set_template_from_resource (widget_class,
                                                KGX_APPLICATION_PATH "kgx-window.ui");
 
-  gtk_widget_class_bind_template_child_private (widget_class, KgxWindow, window_title);
   gtk_widget_class_bind_template_child_private (widget_class, KgxWindow, theme_switcher);
-  gtk_widget_class_bind_template_child_private (widget_class, KgxWindow, zoom_level);
   gtk_widget_class_bind_template_child_private (widget_class, KgxWindow, tab_bar);
   gtk_widget_class_bind_template_child_private (widget_class, KgxWindow, tab_overview);
   gtk_widget_class_bind_template_child_private (widget_class, KgxWindow, pages);
@@ -386,220 +545,41 @@ kgx_window_class_init (KgxWindowClass *klass)
   gtk_widget_class_bind_template_callback (widget_class, create_tab_cb);
   gtk_widget_class_bind_template_callback (widget_class, breakpoint_applied);
   gtk_widget_class_bind_template_callback (widget_class, breakpoint_unapplied);
-}
+  gtk_widget_class_bind_template_callback (widget_class, title_or_fallback);
+  gtk_widget_class_bind_template_callback (widget_class, path_as_subtitle);
+  gtk_widget_class_bind_template_callback (widget_class, scale_as_label);
 
+  gtk_widget_class_install_action (widget_class, "tab.close", NULL, close_tab_activated);
+  gtk_widget_class_install_action (widget_class, "tab.detach", NULL, detach_tab_activated);
 
-static gboolean
-scale_to_label (GBinding     *binding,
-                const GValue *from_value,
-                GValue       *to_value,
-                gpointer      user_data)
-{
-  int zoom = round (g_value_get_double (from_value) * 100);
-
-  g_value_take_string (to_value, g_strdup_printf ("%i%%", zoom));
-
-  return TRUE;
-}
-
-
-static void
-new_activated (GSimpleAction *action,
-               GVariant      *parameter,
-               gpointer       data)
-{
-  KgxWindow *self = data;
-  guint32 timestamp = GDK_CURRENT_TIME;
-  GtkApplication *application = NULL;
-  g_autoptr (GFile) dir = NULL;
-
-  application = gtk_window_get_application (GTK_WINDOW (self));
-  dir = kgx_window_get_working_dir (KGX_WINDOW (data));
-
-  kgx_application_add_terminal (KGX_APPLICATION (application),
-                                NULL,
-                                timestamp,
-                                dir,
-                                NULL,
-                                NULL);
-}
-
-
-static void
-new_tab_activated (GSimpleAction *action,
-                   GVariant      *parameter,
-                   gpointer       data)
-{
-  KgxWindow *self = data;
-  guint32 timestamp = GDK_CURRENT_TIME;
-  GtkApplication *application = NULL;
-  g_autoptr (GFile) dir = NULL;
-
-  application = gtk_window_get_application (GTK_WINDOW (self));
-  dir = kgx_window_get_working_dir (KGX_WINDOW (data));
-
-  kgx_application_add_terminal (KGX_APPLICATION (application),
-                                self,
-                                timestamp,
-                                dir,
-                                NULL,
-                                NULL);
-}
-
-
-static void
-close_tab_activated (GSimpleAction *action,
-                     GVariant      *parameter,
-                     gpointer       data)
-{
-  KgxWindow *self = data;
-  KgxWindowPrivate *priv = kgx_window_get_instance_private (self);
-
-  kgx_pages_close_page (KGX_PAGES (priv->pages));
-}
-
-
-static void
-detach_tab_activated (GSimpleAction *action,
-                      GVariant      *parameter,
-                      gpointer       data)
-{
-  KgxWindow *self = data;
-  KgxWindowPrivate *priv = kgx_window_get_instance_private (self);
-
-  kgx_pages_detach_page (KGX_PAGES (priv->pages));
-}
-
-
-static void
-about_activated (GSimpleAction *action,
-                 GVariant      *parameter,
-                 gpointer       data)
-{
-  const char *developers[] = { "Zander Brown <zbrown@gnome.org>", NULL };
-  const char *designers[] = { "Tobias Bernard", NULL };
-  g_autofree char *copyright = NULL;
-
-  /* Translators: %s is the year range */
-  copyright = g_strdup_printf (_("© %s Zander Brown"), "2019-2023");
-
-  adw_show_about_window (GTK_WINDOW (data),
-                         "application-name", KGX_DISPLAY_NAME,
-                         "application-icon", KGX_APPLICATION_ID,
-                         "developer-name", _("The GNOME Project"),
-                         "issue-url", "https://gitlab.gnome.org/GNOME/console/-/issues/new",
-                         "website", "https://apps.gnome.org/en-GB/app/org.gnome.Console/",
-                         "version", PACKAGE_VERSION,
-                         "developers", developers,
-                         "designers", designers,
-                         /* Translators: Credit yourself here */
-                         "translator-credits", _("translator-credits"),
-                         "copyright", copyright,
-                         "license-type", GTK_LICENSE_GPL_3_0,
-                         NULL);
-}
-
-
-static void
-tab_switcher_activated (GSimpleAction *action,
-                        GVariant      *parameter,
-                        gpointer       data)
-{
-  KgxWindow *self = data;
-  KgxWindowPrivate *priv = kgx_window_get_instance_private (self);
-
-  adw_tab_overview_set_open (ADW_TAB_OVERVIEW (priv->tab_overview), TRUE);
-}
-
-
-static void
-show_preferences_window_activated (GSimpleAction *action,
-                                   GVariant      *parameter,
-                                   gpointer       data)
-{
-  KgxWindow *self = data;
-  KgxWindowPrivate *priv = kgx_window_get_instance_private (self);
-  GtkWindow *preferences;
-
-  preferences = g_object_new (KGX_TYPE_PREFERENCES_WINDOW,
-                              "transient-for", self,
-                              "settings", priv->settings,
-                              NULL);
-  gtk_window_present (preferences);
-}
-
-
-static GActionEntry win_entries[] = {
-  { "new-window", new_activated, NULL, NULL, NULL },
-  { "new-tab", new_tab_activated, NULL, NULL, NULL },
-  { "close-tab", close_tab_activated, NULL, NULL, NULL },
-  { "about", about_activated, NULL, NULL, NULL },
-  { "show-tabs", tab_switcher_activated, NULL, NULL, NULL },
-  { "show-tabs-desktop", tab_switcher_activated, NULL, NULL, NULL },
-  { "show-preferences-window", show_preferences_window_activated, NULL, NULL, NULL },
-};
-
-
-static GActionEntry tab_entries[] = {
-  { "close", close_tab_activated, NULL, NULL, NULL },
-  { "detach", detach_tab_activated, NULL, NULL, NULL },
-};
-
-
-static gboolean
-update_title (GBinding     *binding,
-              const GValue *from_value,
-              GValue       *to_value,
-              gpointer      data)
-{
-  const char *title = g_value_get_string (from_value);
-
-  if (!title) {
-    title = KGX_DISPLAY_NAME;
-  }
-
-  g_value_set_string (to_value, title);
-
-  return TRUE;
-}
-
-
-static gboolean
-update_subtitle (GBinding     *binding,
-                 const GValue *from_value,
-                 GValue       *to_value,
-                 gpointer      data)
-{
-  g_autoptr (GFile) file = NULL;
-  g_autofree char *path = NULL;
-  const char *home = NULL;
-
-  file = g_value_dup_object (from_value);
-  if (file == NULL) {
-    g_value_set_string (to_value, NULL);
-    return TRUE;
-  }
-
-  path = g_file_get_path (file);
-  if (path == NULL) {
-    g_value_set_string (to_value, NULL);
-
-    return TRUE;
-  }
-
-  home = g_get_home_dir ();
-  if (g_str_has_prefix (path, home)) {
-    g_autofree char *short_home = g_strdup_printf ("~%s",
-                                                   path + strlen (home));
-
-    g_value_set_string (to_value, short_home);
-
-    return TRUE;
-  }
-
-  g_value_set_string (to_value, path);
-
-  return TRUE;
+  gtk_widget_class_install_action (widget_class,
+                                   "win.new-window",
+                                   NULL,
+                                   new_activated);
+  gtk_widget_class_install_action (widget_class,
+                                   "win.new-tab",
+                                   NULL,
+                                   new_tab_activated);
+  gtk_widget_class_install_action (widget_class,
+                                   "win.close-tab",
+                                   NULL,
+                                   close_tab_activated);
+  gtk_widget_class_install_action (widget_class,
+                                   "win.about",
+                                   NULL,
+                                   about_activated);
+  gtk_widget_class_install_action (widget_class,
+                                   "win.show-tabs",
+                                   NULL,
+                                   tab_switcher_activated);
+  gtk_widget_class_install_action (widget_class,
+                                   "win.show-tabs-desktop",
+                                   NULL,
+                                   tab_switcher_activated);
+  gtk_widget_class_install_action (widget_class,
+                                   "win.show-preferences-window",
+                                   NULL,
+                                   show_preferences_window_activated);
 }
 
 
@@ -625,31 +605,9 @@ kgx_window_init (KgxWindow *self)
                         priv->theme_switcher, "theme",
                         G_BINDING_SYNC_CREATE | G_BINDING_BIDIRECTIONAL);
 
-  g_binding_group_bind_full (priv->settings_binds, "font-scale",
-                             priv->zoom_level, "label",
-                             G_BINDING_SYNC_CREATE | G_BINDING_BIDIRECTIONAL,
-                             scale_to_label, NULL, NULL, NULL);
-
-  g_action_map_add_action_entries (G_ACTION_MAP (self),
-                                   win_entries,
-                                   G_N_ELEMENTS (win_entries),
-                                   self);
-
   #ifdef IS_DEVEL
   gtk_widget_add_css_class (GTK_WIDGET (self), "devel");
   #endif
-
-  g_object_bind_property_full (priv->pages, "title",
-                               self, "title",
-                               G_BINDING_SYNC_CREATE,
-                               update_title,
-                               NULL, NULL, NULL);
-
-  g_object_bind_property_full (priv->pages, "path",
-                               priv->window_title, "subtitle",
-                               G_BINDING_SYNC_CREATE,
-                               update_subtitle,
-                               NULL, NULL, NULL);
 
   /* Note this unfortunately doesn't allow us to workaround the portal
      situation, but hopefully dropping folders on tabs is relatively rare */
@@ -664,15 +622,6 @@ kgx_window_init (KgxWindow *self)
 
   group = gtk_window_group_new ();
   gtk_window_group_add_window (group, GTK_WINDOW (self));
-
-  priv->tab_actions = G_ACTION_MAP (g_simple_action_group_new ());
-  g_action_map_add_action_entries (priv->tab_actions,
-                                   tab_entries,
-                                   G_N_ELEMENTS (tab_entries),
-                                   self);
-  gtk_widget_insert_action_group (GTK_WIDGET (self),
-                                  "tab",
-                                  G_ACTION_GROUP (priv->tab_actions));
 }
 
 
