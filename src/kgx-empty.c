ca40c493c47b917f8ce4deac0a7286037a6b7569
Zander Brown <zbrown@gnome.org>
2025-04-19 06:15:58 +0100

    empty: use properties to control widgets
    This allows us to test our timeouts by observing properties

--- src/kgx-empty.c
+++ src/kgx-empty.c
@@ -1,6 +1,6 @@
 /* kgx-empty.c
  *
- * Copyright 2024 Zander Brown
+ * Copyright 2024-2025 Zander Brown
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -19,6 +19,7 @@
 #include "kgx-config.h"
 
 #include <gtk/gtk.h>
+#include <adwaita.h>
 
 #include "kgx-utils.h"
 
@@ -28,15 +29,15 @@
 struct _KgxEmpty {
   GtkBox       parent_instance;
 
-  GtkRevealer *image_revealer;
-  GtkRevealer *spinner_revealer;
+  gboolean     working;
 
-  guint        image_timeout;
+  gboolean     logo_visible;
+  guint        logo_timeout;
+
+  gboolean     spinner_visible;
   guint        spinner_timeout;
 
   gboolean     had_first_map;
-
-  gboolean     working;
 };
 
 
@@ -46,6 +47,8 @@ G_DEFINE_FINAL_TYPE (KgxEmpty, kgx_empty, GTK_TYPE_BOX)
 enum {
   PROP_0,
   PROP_WORKING,
+  PROP_LOGO_VISIBLE,
+  PROP_SPINNER_VISIBLE,
   LAST_PROP
 };
 static GParamSpec *pspecs[LAST_PROP] = { NULL, };
@@ -56,7 +59,7 @@ kgx_empty_dispose (GObject *object)
 {
   KgxEmpty *self = KGX_EMPTY (object);
 
-  g_clear_handle_id (&self->image_timeout, g_source_remove);
+  g_clear_handle_id (&self->logo_timeout, g_source_remove);
   g_clear_handle_id (&self->spinner_timeout, g_source_remove);
 
   G_OBJECT_CLASS (kgx_empty_parent_class)->dispose (object);
@@ -75,9 +78,13 @@ kgx_empty_get_property (GObject    *object,
     case PROP_WORKING:
       g_value_set_boolean (value, self->working);
       break;
-    default:
-      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+    case PROP_LOGO_VISIBLE:
+      g_value_set_boolean (value, self->logo_visible);
       break;
+    case PROP_SPINNER_VISIBLE:
+      g_value_set_boolean (value, self->spinner_visible);
+      break;
+    KGX_INVALID_PROP (object, property_id, pspec);
   }
 }
 
@@ -87,7 +94,9 @@ show_spinner (gpointer user_data)
 {
   KgxEmpty *self = KGX_EMPTY (user_data);
 
-  gtk_revealer_set_reveal_child (self->spinner_revealer, TRUE);
+  self->spinner_visible = TRUE;
+  g_object_notify_by_pspec (G_OBJECT (self), pspecs[PROP_SPINNER_VISIBLE]);
+
   self->spinner_timeout = 0;
 }
 
@@ -100,7 +109,8 @@ update_spinner (KgxEmpty *self)
   if (self->working) {
     self->spinner_timeout = g_timeout_add_once (100, show_spinner, self);
   } else {
-    gtk_revealer_set_reveal_child (self->spinner_revealer, FALSE);
+    self->spinner_visible = FALSE;
+    g_object_notify_by_pspec (G_OBJECT (self), pspecs[PROP_SPINNER_VISIBLE]);
   }
 }
 
@@ -119,20 +129,20 @@ kgx_empty_set_property (GObject      *object,
         update_spinner (self);
       }
       break;
-    default:
-      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
-      break;
+    KGX_INVALID_PROP (object, property_id, pspec);
   }
 }
 
 
 static void
-show_image (gpointer user_data)
+show_logo (gpointer user_data)
 {
   KgxEmpty *self = KGX_EMPTY (user_data);
 
-  gtk_revealer_set_reveal_child (self->image_revealer, TRUE);
-  self->image_timeout = 0;
+  self->logo_visible = TRUE;
+  g_object_notify_by_pspec (G_OBJECT (self), pspecs[PROP_LOGO_VISIBLE]);
+
+  self->logo_timeout = 0;
 }
 
 
@@ -143,7 +153,7 @@ kgx_empty_map (GtkWidget *widget)
 
   if (!self->had_first_map) {
     self->had_first_map = TRUE;
-    self->image_timeout = g_timeout_add_once (80, show_image, self);
+    self->logo_timeout = g_timeout_add_once (80, show_logo, self);
   }
 
   GTK_WIDGET_CLASS (kgx_empty_parent_class)->map (widget);
@@ -155,7 +165,7 @@ kgx_empty_unmap (GtkWidget *widget)
 {
   KgxEmpty *self = KGX_EMPTY (widget);
 
-  g_clear_handle_id (&self->image_timeout, g_source_remove);
+  g_clear_handle_id (&self->logo_timeout, g_source_remove);
   g_clear_handle_id (&self->spinner_timeout, g_source_remove);
 
   GTK_WIDGET_CLASS (kgx_empty_parent_class)->unmap (widget);
@@ -180,13 +190,22 @@ kgx_empty_class_init (KgxEmptyClass *klass)
                           FALSE,
                           G_PARAM_READWRITE | G_PARAM_EXPLICIT_NOTIFY | G_PARAM_STATIC_STRINGS);
 
+  pspecs[PROP_LOGO_VISIBLE] =
+    g_param_spec_boolean ("logo-visible", NULL, NULL,
+                          FALSE,
+                          G_PARAM_READABLE | G_PARAM_EXPLICIT_NOTIFY | G_PARAM_STATIC_STRINGS);
+
+  pspecs[PROP_SPINNER_VISIBLE] =
+    g_param_spec_boolean ("spinner-visible", NULL, NULL,
+                          FALSE,
+                          G_PARAM_READABLE | G_PARAM_EXPLICIT_NOTIFY | G_PARAM_STATIC_STRINGS);
+
   g_object_class_install_properties (object_class, LAST_PROP, pspecs);
+
+  g_type_ensure (ADW_TYPE_SPINNER);
 
   gtk_widget_class_set_template_from_resource (widget_class,
                                                KGX_APPLICATION_PATH "kgx-empty.ui");
-
-  gtk_widget_class_bind_template_child (widget_class, KgxEmpty, image_revealer);
-  gtk_widget_class_bind_template_child (widget_class, KgxEmpty, spinner_revealer);
 
   gtk_widget_class_set_css_name (widget_class, "kgx-empty");
 }
