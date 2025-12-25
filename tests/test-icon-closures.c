0d9c996657f1900d8a328109f8e522d504005057
Zander Brown <zbrown@gnome.org>
2025-12-25 22:36:07 +0000

    tests: increase KgxStatus coverage

--- tests/test-icon-closures.c
+++ tests/test-icon-closures.c
@@ -18,35 +18,37 @@
 
 #include "kgx-config.h"
 
+#include "kgx-enums.h"
 #include "kgx-train.h"
 
 #include "kgx-icon-closures.h"
 
 
+struct status_as_icon_test {
+  KgxStatus status;
+  const char *expected;
+} status_as_icon_cases[] = {
+  { KGX_REMOTE, "status-remote-symbolic" },
+  { KGX_PLAYBOX, "status-playbox-symbolic" },
+  { KGX_PRIVILEGED, "status-privileged-symbolic" },
+  { KGX_REMOTE | KGX_PRIVILEGED, "status-remote-symbolic" },
+  { KGX_NONE, NULL },
+};
+
+
 static void
-test_status_as_icon (void)
+test_status_as_icon (gconstpointer user_data)
 {
-  g_autoptr (GIcon) remote_icon = kgx_status_as_icon (NULL, KGX_REMOTE);
-  g_autoptr (GIcon) root_icon = kgx_status_as_icon (NULL, KGX_PRIVILEGED);
-  g_autoptr (GIcon) remote_root_icon =
-    kgx_status_as_icon (NULL, KGX_REMOTE | KGX_PRIVILEGED);
+  const struct status_as_icon_test *test = user_data;
+  g_autoptr (GIcon) icon = kgx_status_as_icon (NULL, test->status);
 
-  g_assert_nonnull (remote_icon);
-  g_assert_true (G_IS_THEMED_ICON (remote_icon));
-  g_assert_true (g_strv_contains (g_themed_icon_get_names (G_THEMED_ICON (remote_icon)),
-                                  "status-remote-symbolic"));
-
-  g_assert_nonnull (root_icon);
-  g_assert_true (G_IS_THEMED_ICON (root_icon));
-  g_assert_true (g_strv_contains (g_themed_icon_get_names (G_THEMED_ICON (root_icon)),
-                                  "status-privileged-symbolic"));
-
-  g_assert_nonnull (remote_root_icon);
-  g_assert_true (G_IS_THEMED_ICON (remote_root_icon));
-  g_assert_true (g_strv_contains (g_themed_icon_get_names (G_THEMED_ICON (remote_root_icon)),
-                                  "status-remote-symbolic"));
-
-  g_assert_null (kgx_status_as_icon (NULL, KGX_NONE));
+  if (test->expected) {
+    g_assert_nonnull (icon);
+    g_assert_true (g_strv_contains (g_themed_icon_get_names (G_THEMED_ICON (icon)),
+                                    test->expected));
+  } else {
+    g_assert_null (icon);
+  }
 }
 
 
@@ -69,7 +71,14 @@ main (int argc, char *argv[])
 {
   g_test_init (&argc, &argv, NULL);
 
-  g_test_add_func ("/kgx/icon-closures/status_as_icon", test_status_as_icon);
+  for (size_t i = 0; i < G_N_ELEMENTS (status_as_icon_cases); i++) {
+    g_autofree char *name =
+      g_flags_to_string (KGX_TYPE_STATUS, status_as_icon_cases[i].status);
+    g_autofree char *path =
+      g_strconcat ("/kgx/icon-closures/status_as_icon/", name, NULL);
+
+    g_test_add_data_func (path, &status_as_icon_cases[i], test_status_as_icon);
+  }
   g_test_add_func ("/kgx/icon-closures/ringing_as_icon", test_ringing_as_icon);
 
   return g_test_run ();
